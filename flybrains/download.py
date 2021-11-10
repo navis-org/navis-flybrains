9c347a9a32312ce66def569470e5d03f1625ad52
Rob Court <robbie.court@gmail.com>
2021-11-10 12:55:19 +0000

    adding bridging desc

--- flybrains/download.py
+++ flybrains/download.py
@@ -30,9 +30,11 @@ def download_vfb_transforms(repos=("VfbBridgingRegistrations"),
     """Download VirtualFlyBrain.org (VFB) CMTK transforms.
 
     BridgingRegistrations (~5Mb):
-      - JRC2018_VNC_UNISEX_4iso <-> JRC2018_VNC_FEMALE_4iso
+      - JRC2018VU-JRC2018VU: JRC2018-VNC-UNISEX-4iso -> JRC2018-VNC-FEMALE-4iso
+      - JRC2018VM-JRC2018VU: JRC2018_VNC_MALE_4iso -> JRC2018_VNC_UNISEX_4iso
+      - Court2017VNS-JRC2018VF: DrosAdultVNSdomains_Court2017_template_Neuropil_LPS -> JRC2018_VNC_FEMALE_4iso_LPS
+      - flyVNCtemplateFs-JRC2018VU: 20x_flyVNCtemplate_Female_symmetric -> JRC2018_VNC_UNISEX_4iso
 
-    
 
     Parameters
     ----------
