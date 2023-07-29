45577dd8e9e7168e6ba866c0b648ca27862cdf8e
Philipp Schlegel <observing@web.de>
2023-07-29 06:32:50 +0100

    bump version 0.2.7 -> 0.2.8

--- flybrains/__version__.py
+++ flybrains/__version__.py
@@ -11,5 +11,5 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
-__version__ = '0.2.7'
-__version_vector__ = (0, 2, 7)
+__version__ = '0.2.8'
+__version_vector__ = (0, 2, 8)
